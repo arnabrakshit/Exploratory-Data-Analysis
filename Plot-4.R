a0f8230211734a6505d0b57bae07c4aa1bb26845
arnabrakshit <rakshit.arnab@gmail.com>
2015-04-13 19:15:28 +0530

    Update Plot-4.R

--- Plot-4.R
+++ Plot-4.R
@@ -1,29 +1,26 @@
-data<- read.table("household_power_consumption.txt", sep=";", header=T, quote= "", strip.white=TRUE, stringsAsFactors = F, na.strings= "?")
+source("downloadArchive.R")
 
-# Subsetting the full data to obtain the data related to two days: 
-data<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
-# change the date format
-data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
-data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+# Subset coal combustion related NEI data
+combustionRelated <- grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
+coalCombustion <- (combustionRelated & coalRelated)
+combustionSCC <- SCC[coalCombustion,]$SCC
+combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
 
+png("plot4.png",width=480,height=480,units="px",bg="transparent")
 
+library(ggplot2)
 
-# creating Plot4
-png("plot4.png", width = 480, height = 480)
+ggp <- ggplot(combustionNEI,aes(factor(year),Emissions/10^5)) +
+  geom_bar(stat="identity",fill="grey",width=0.75) +
+  theme_bw() +  guides(fill=FALSE) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
 
-# divide the plot device into four subplots
-par(mfrow=c(2,2)) 
-# top left
-plot(data$DateTime, data$Global_active_power, type= "l", lwd=1, ylab= "Global Active Power (kilowatts)", xlab="") 
-# top right
-plot(data$DateTime, data$Voltage, type="l", xlab="datetime", ylab="Voltage")   
-# bottom left
-plot(data$DateTime, data$Sub_metering_1, type="l", ylab= "Energy sub metering", xlab="")
-lines(data$DateTime, data$Sub_metering_2, type="l", col="red")
-lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, bty="n", col=c("black", "red", "blue")) 
-# bottom right
-plot(data$DateTime, data$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")  
+print(ggp)
 
 dev.off()
